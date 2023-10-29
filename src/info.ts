481aad4fa52dfc11ad4e10ec16428b5a36624ed2
Abel Naharro <abelnhm92@gmail.com>
2023-10-29 22:22:44 +0100

    End review with router, test and pokemon info

--- src/info.ts
+++ src/info.ts
@@ -2,13 +2,10 @@ import './scss/style.scss';
 
 import { Header } from './components/header';
 import { Main } from './components/main';
-import { List } from './components/list';
 import { Router } from './components/router';
 import { Footer } from './components/footer';
 
 function main() {
-  console.log('Loaded main');
-
   const appElement = document.querySelector<HTMLDivElement>('#app');
   if (appElement === null) return;
 
@@ -16,7 +13,6 @@ function main() {
     new Main('#app'),
     new Header('main', 'Pokémon'),
     new Router('main'),
-    new List('main'),
     new Footer('#app', 'By Abel Naharro'),
   ];
 
